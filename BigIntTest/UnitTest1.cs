a945ee2516cdc898e6bdc580db1fa45c91bdda57
Mew_boy33 <pugach@dlit.dp.ua>
2019-08-22 23:36:38 +0300

    Added several tests

--- BigIntTest/UnitTest1.cs
+++ BigIntTest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Cryptography;
 using System.Numerics;
+using System;
 
 namespace Tests
 {
@@ -10,15 +11,41 @@ namespace Tests
         public void Setup()
         {
         }
-
-        [Test]
-        public void Test1()
+        
+        [Description("Checking initialization and overriding ToString() method")]
+        [TestCase("1213243234")]
+        [TestCase("12345611111")]
+        [TestCase("1213243234a", Ignore = "Code is not complete yet")]
+        [TestCase("123^2", Ignore = "Code is not complete yet")]
+        [MaxTime(2000)]
+        public void Test1(string text_number)
         {
-            string expectedResult = "12345";
+            // Arrange
+            BigInt bigInt = new BigInt(text_number);
 
-            BigInt bigInt = new BigInt(12345);
+            // Act
+            bool equal = bigInt.ToString() == text_number;
 
-            Assert.AreEqual(true, bigInt.ToString() == expectedResult);
+            // Assert
+            Assert.IsTrue(equal);
+        }
+        
+        [TestCase("1289832894318943289032489", "9240384329034318943289032489")]
+        public void Test2(string num1, string num2)
+        {
+            // Arrange
+            BigInt b1 = new BigInt(num1);
+            BigInt b2 = new BigInt(num2);
+
+            BigInteger b1_2 = new BigInteger(Convert.ToDecimal(num1));
+            BigInteger b2_2 = new BigInteger(Convert.ToDecimal(num2));
+            
+            // Act
+            BigInt b3 = b1 + b2;            
+            BigInteger b3_2 = b1_2 + b2_2;
+            
+            // Assert
+            Assert.AreEqual(b3_2.ToString(), b3.ToString());
         }
     }
 }
