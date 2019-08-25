7689a10b3818873892adad6d7e89619d35660b58
Mew_boy33 <pugach@dlit.dp.ua>
2019-08-25 22:36:52 +0300

    Tests for static functions added

---
+++ BigIntTest/StaticMethodsTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using Cryptography;
+using System.Numerics;
+using System;
+
+
+namespace Tests
+{
+    partial class Tests
+    {
+        [TestCase("3", "9")]
+        [TestCase("391", "609")]
+        [TestCase("39190329032", "329012609")]
+        [TestCase("1289832894318943289032489", "9240384329034318943289032489")]
+        [TestCase("-9", "4332")]
+        public void Adding_strings(string num1, string num2)
+        {
+            // Arrange
+            BigInt b1 = new BigInt(num1);
+            BigInt b2 = new BigInt(num2);
+
+            BigInteger b1_2 = new BigInteger(Convert.ToDecimal(num1));
+            BigInteger b2_2 = new BigInteger(Convert.ToDecimal(num2));
+
+            // Act
+            BigInt b3 = b1 + b2;
+            BigInteger b3_2 = b1_2 + b2_2;
+
+            // Assert
+            Assert.AreEqual(b3_2.ToString(), b3.ToString());
+        }
+
+        [TestCase("29", "9")]
+        [TestCase("9", "29")]
+        [TestCase("239023", "43234")]
+        [TestCase("1289832894318943289032489", "9240384329034318943289032489")]
+        [TestCase("-438934", "89342432")]
+        public void Minus_strings(string num1, string num2)
+        {
+            // Arrange
+            BigInt b1 = new BigInt(num1);
+            BigInt b2 = new BigInt(num2);
+
+            BigInteger b1_2 = new BigInteger(Convert.ToDecimal(num1));
+            BigInteger b2_2 = new BigInteger(Convert.ToDecimal(num2));
+
+            // Act
+            BigInt b3 = b1 - b2;
+            BigInteger b3_2 = b1_2 - b2_2;
+
+            // Assert
+            Assert.AreEqual(b3_2.ToString(), b3.ToString());
+        }
+    }
+}
--- BigIntTest/UnitTest1.cs
+++ BigIntTest/UnitTest1.cs
@@ -5,7 +5,7 @@ using System;
 
 namespace Tests
 {
-    public class Tests
+    public partial class Tests
     {
         [SetUp]
         public void Setup()
@@ -28,24 +28,6 @@ namespace Tests
 
             // Assert
             Assert.IsTrue(equal);
-        }
-        
-        [TestCase("1289832894318943289032489", "9240384329034318943289032489")]
-        public void Test2(string num1, string num2)
-        {
-            // Arrange
-            BigInt b1 = new BigInt(num1);
-            BigInt b2 = new BigInt(num2);
-
-            BigInteger b1_2 = new BigInteger(Convert.ToDecimal(num1));
-            BigInteger b2_2 = new BigInteger(Convert.ToDecimal(num2));
-            
-            // Act
-            BigInt b3 = b1 + b2;            
-            BigInteger b3_2 = b1_2 + b2_2;
-            
-            // Assert
-            Assert.AreEqual(b3_2.ToString(), b3.ToString());
         }
     }
 }
